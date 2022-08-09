d662d78faf784c0b341f83058071b5acc1d1a3e1
Your Name <you@example.com>
2022-08-09 09:59:56 +0000

    decrease learning rate b/c we're stallin

--- run_scripts/1xA100-80GB.bash
+++ run_scripts/1xA100-80GB.bash
@@ -4,8 +4,8 @@ set -o xtrace
 FILE_ARGS='--config-file=./config/fixed_variance.yaml --dir-train=~/dataset/train --dir-val=~/dataset/val'
 TIME_ARGS='--target-time=10hr --batch-rate=1'
 # Original batch size in paper is 128
-# Scale learning rate linearly to batch size
-SCHEDULE_ARGS='--warmup=500 --lr="2.3*1e-4"'
+# Scale learning rate by sqrt to batch size
+SCHEDULE_ARGS='--warmup=500 --lr="1.51*1e-4"'
 BATCH_ARGS='--batch-size=302 --micro-batches=1'
 METRICS_ARGS='--checkpoints=12 --diffusion-logs=200 --evals=120'
 
